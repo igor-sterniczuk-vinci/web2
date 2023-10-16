51d61e20b9396d82d341f9162843f963fc61589b
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2023-10-16 12:04:42 +0200

    1.6 : API : delete & modify.

---
+++ exercises/1.6/basic/routes/films.js
@@ -0,0 +1,171 @@
+var express = require('express');
+var router = express.Router();
+
+const films = [
+  {
+    id: 1,
+    title: 'Star Wars: The Phantom Menace (Episode I)',
+    duration: 136,
+    budget: '115',
+    link: 'https://en.wikipedia.org/wiki/Star_Wars:_Episode_I_%E2%80%93_The_Phantom_Menace',
+  },
+  {
+    id: 2,
+    title: 'Star Wars: Episode II – Attack of the Clones',
+    duration: 142,
+    budget: 115,
+    link: 'https://en.wikipedia.org/wiki/Star_Wars:_Episode_II_%E2%80%93_Attack_of_the_Clones',
+  },
+  {
+    id: 3,
+    title: "Zack Snyder's Justice League",
+    duration: 242,
+    budget: 70,
+    link: 'https://en.wikipedia.org/wiki/Zack_Snyder%27s_Justice_League',
+  },
+];
+
+// Read all the films, filtered by minimum-duration if the query param exists
+router.get('/', (req, res) => {
+  const minimumFilmDuration = req?.query?.['minimum-duration']
+    ? Number(req.query['minimum-duration'])
+    : undefined;
+
+  if (minimumFilmDuration === undefined) return res.json(films);
+
+  if (typeof minimumFilmDuration !== 'number' || minimumFilmDuration <= 0)
+    return res.sendStatus(400); // bad practise (will be improved in exercise 1.5)
+
+  const filmsReachingMinimumDuration = films.filter(
+    (film) => film.duration >= minimumFilmDuration
+  );
+  return res.json(filmsReachingMinimumDuration);
+});
+
+// Read a film from its id in the menu
+router.get('/:id', (req, res) => {
+  const indexOfFilmFound = films.findIndex((film) => film.id == req.params.id);
+
+  if (indexOfFilmFound < 0) return res.sendStatus(404); // bad practise (will be improved in exercise 1.5)
+
+  return res.json(films[indexOfFilmFound]);
+});
+
+// Create a film
+router.post('/', (req, res) => {
+  const title =
+    req?.body?.title?.trim()?.length !== 0 ? req.body.title : undefined; //si le la valeur recuperer nest pas vide le titre prend sa valeur sinon undefined
+  const link =
+    req?.body?.content?.trim().length !== 0 ? req.body.link : undefined; //si le la valeur recuperer nest pas vide le titre prend sa valeur sinon undefined
+  const duration =
+    typeof req?.body?.duration !== 'number' || req.body.duration < 0 //on regarde le type de la duration recuperer si ce n'est pas un nombre ou bien si la duration est negative on lui donne la valeur undefineted
+      ? undefined
+      : req.body.duration;
+  const budget =
+    typeof req?.body?.budget !== 'number' || req.body.budget < 0 //on regarde le type de la duration recuperer si ce n'est pas un nombre ou bien si la duration est negative on lui donne la valeur undefineted
+      ? undefined
+      : req.body.budget;
+
+  if (!title || !link || !duration || !budget) return res.sendStatus(400); // bad practise (will be improved in exercise 1.5) //verification de toutes les variables, si une des variables nest pas bonne on renvoie erreur 400
+
+  const lastItemIndex = films?.length !== 0 ? films.length - 1 : undefined; //recherche de l'indice le plus grand de la table
+  const lastId = lastItemIndex !== undefined ? films[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  const newFilm = { id: nextId, title, link, duration, budget }; //creation dun nouveau film
+
+  films.push(newFilm); //ajout du nouveasu film
+
+  return res.json(newFilm); //affichage du nouveau film
+});
+
+
+router.delete('/:id', (req,res) =>{
+  console.log('delete /films/${req.params.id}');
+
+  const indexFound = films.findIndex(film => film.id == req.params.id);
+
+  if (indexFound<0) return res.sendStatus(404);
+
+  const itemsRemovedFromMenu = films.splice(indexFound,1);
+  const itemRemoved = itemsRemovedFromMenu[0];
+
+  res.json(itemRemoved)
+});
+
+router.patch('/:id', (req,res)=>{
+  const title = req?.body?.title;
+  const duration = req?.body?.duration;
+  const budget = req?.body?.budget;
+  const link = req?.body?.link;
+
+  console.log('POST films')
+
+  if (
+    !req.body ||
+    (title !== undefined && !title.trim()) ||
+    (link !== undefined && !link.trim()) ||
+    (duration !== undefined && (typeof req?.body?.duration !== 'number' || duration < 0)) ||
+    (budget !== undefined && (typeof req?.body?.budget !== 'number' || budget < 0))
+  )
+    return res.sendStatus(400);
+
+  const foundIndex = films.findIndex((film) => film.id == req.params.id);
+  if (foundIndex<0) return res.sendStatus(404);
+
+  const filmPriorChange = films[foundIndex];
+  const objContainingProperties = req.body;
+
+  const updatedFilm = {...filmPriorChange,...objContainingProperties};
+  
+  films[foundIndex] = updatedFilm;
+
+  return res.json(updatedFilm);
+})
+
+
+
+router.put('/:id', function (req, res) {
+  const title = req?.body?.title;
+  const link = req?.body?.link;
+  const duration = req?.body?.duration;
+  const budget = req?.body?.budget;
+
+  if (
+    !req.body ||
+    !title ||
+    !title.trim() ||
+    !link ||
+    !link.trim() ||
+    duration === undefined ||
+    typeof req?.body?.duration !== 'number' ||
+    duration < 0 ||
+    budget === undefined ||
+    typeof req?.body?.budget !== 'number' ||
+    budget < 0
+  )
+    return res.sendStatus(400);
+
+  const id = req.params.id;
+  const indexOfFilmFound = films.findIndex((film) => film.id == id);
+
+  if (indexOfFilmFound < 0) {
+    const newFilm = { id, title, link, duration, budget };
+    films.push(newFilm);
+    return res.json(newFilm);
+  }
+
+  const filmPriorToChange = films[indexOfFilmFound];
+  const objectContainingPropertiesToBeUpdated = req.body;
+
+  const updatedFilm = {
+    ...filmPriorToChange,
+    ...objectContainingPropertiesToBeUpdated,
+  };
+
+  films[indexOfFilmFound] = updatedFilm;
+
+  return res.json(updatedFilm);
+});
+
+module.exports = router;
